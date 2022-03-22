0f942f621487ea191abb716cd6a6714d296bf92a
LNFrettes <ln.frettes@gmail.com>
2022-03-22 13:40:24 -0300

    it s with dotenv

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux'
 import store from './Store/index'
 import axios from 'axios';
-// require('dotenv').config();
+require('dotenv').config();
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
